0627725e1ee5dac37fb36027453771331889fbdc
Musk <yuiop1029@naver.com>
2025-05-25 17:55:07 +0900

    Initial Commit

---
+++ TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by yooinho on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
